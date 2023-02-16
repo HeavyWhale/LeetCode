313156fa77bb299d61503fcfe45c6ebbe06eb801
Jinyu Chen <rzco0717@gmail.com>
2023-02-16 10:30:47 -0500

    practice @ Feb 16

---
+++ 238_Product_of_Array_Except_Self/main.cc
@@ -0,0 +1,11 @@
+#include "prac5.cc"
+
+int main() {
+    vector<int>
+        ex1 {1,2,3,4},
+        ex2 {-1,1,0,-3,3};
+    
+    Solution so {};
+    cout << repr(so.productExceptSelf(ex1)) << '\n';    // [24|12|8|6]
+    cout << repr(so.productExceptSelf(ex2)) << '\n';    // [0|0|9|0|0]
+}
