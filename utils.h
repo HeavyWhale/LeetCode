1bf01e69732c657d4c25f4c2f57cc77e58dbb137
Jinyu Chen <rzco0717@gmail.com>
2023-02-20 13:20:41 -0500

    support more containers & attempt iterator impl

--- utils.h
+++ utils.h
@@ -1,10 +1,12 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <set>
+#include <map>
 #include <stack>
 #include <queue>
 
-#include <algorithm>        // swap, max, min, transform, sort, max_element, min_element
+#include <algorithm>        // swap, max, min, transform, sort, max_element, min_element, for_each
 
 #include <string>
 #include <iostream>
@@ -14,13 +16,15 @@
 
 #include <climits>          // INT_MAX, INT_MIN
 
+#include <__iterator/iterator_traits.h>
+
 // containers
 using std::vector, std::unordered_map, std::unordered_set, std::stack, 
-    std::queue, std::pair, std::tuple;
+    std::queue, std::pair, std::tuple, std::map, std::set;
 // functions
 using std::swap, std::max, std::min, std::function, std::apply, std::stoi,
     std::make_pair, std::transform, std::sort, std::back_inserter,
-    std::min_element, std::max_element;
+    std::min_element, std::max_element, std::for_each;
 // io-related
 using std::string, std::cout, std::endl, std::stringstream, std::to_string;
 
@@ -119,6 +123,42 @@ END:
 }
 
 template<class Key, class Value>
+string repr(const map<Key, Value>& map)
+{
+    stringstream ss;
+    ss << '{';
+    if (map.empty()) goto END;
+    ss << ' ';
+    for (auto it = map.cbegin() ;; ) {
+        ss << repr(it->first) << ':' << repr(it->second);
+        if (++it == map.cend()) break;
+        ss << " | ";
+    }
+    ss << ' ';
+END:
+    ss << '}';
+    return ss.str();
+}
+
+template<class Key>
+string repr(const set<Key>& set)
+{
+    stringstream ss;
+    ss << '{';
+    if (set.empty()) goto END;
+    ss << ' ';
+    for (auto it = set.cbegin() ;; ) {
+        ss << repr(*it);
+        if (++it == set.cend()) break;
+        ss << " | ";
+    }
+    ss << ' ';
+END:
+    ss << '}';
+    return ss.str();
+}
+
+template<class Key, class Value>
 string repr(const unordered_map<Key, Value>& map)
 {
     stringstream ss;
@@ -135,6 +175,48 @@ END:
     ss << '}';
     return ss.str();
 }
+
+template<class Key>
+string repr(const unordered_set<Key>& set)
+{
+    stringstream ss;
+    ss << '{';
+    if (!set.empty()) {
+        ss << ' ';
+        for (auto it = set.cbegin() ;; ) {
+            ss << repr(*it);
+            if (++it == set.cend()) break;
+            ss << " | ";
+        }
+        ss << ' ';
+    }
+    ss << '}';
+    return ss.str();
+}
+
+// FIXME: Only works for iterators corresponding to one item (vector<int>, set<int> for example)
+/*
+template <class _ForwardIterator>
+string repr(_ForwardIterator __first, _ForwardIterator __last)
+{
+    static_assert(std::__is_cpp17_forward_iterator<_ForwardIterator>::value,
+        "std::max_element requires a ForwardIterator");
+    stringstream ss;
+    ss << '{';
+    if (__first != __last) {
+        ss << ' ';
+        for (_ForwardIterator __i = __first ;; ) {
+            ss << repr(*__i);
+            if (++__i == __last) break;
+            ss << '|';
+        }
+        ss << ' ';
+    }
+    ss << '}';
+    return ss.str();
+}
+*/
+
 
 template <typename T>
 string arr_repr(const T* arr, const size_t size)
