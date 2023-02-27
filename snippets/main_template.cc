534832d1ba4d202ca77bb6b04ca4580cc06e6362
Jinyu Chen <rzco0717@gmail.com>
2023-02-26 22:10:14 -0500

    add template for `main.cc`

---
+++ snippets/main_template.cc
@@ -0,0 +1,15 @@
+#include "soln.cc"
+
+int main() {
+    vector<int> 
+        exp1 {},
+        exp2 {},
+        exp3 {};
+    int
+        h1   {},
+        h2   {},
+        h3   {};
+
+    Solution so;
+    cout << so. << '\n';     // 
+}
