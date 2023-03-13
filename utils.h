c0b15440d8abc26c94486cf0ae30a0598096d0f2
Jinyu Chen <rzco0717@gmail.com>
2023-03-13 11:22:17 -0400

    make `phantom` inline to avoid name collision

--- utils.h
+++ utils.h
@@ -282,7 +282,7 @@ string named_repr_of_vars(const char* names, T&& var, Args&&... args) {
 }
 
 // return the `str` but all characters are replaced by space (' ')
-string phantom(const string& str) {
+inline string phantom(const string& str) {
     ostringstream oss;
     for (size_t i = 0; i < str.size(); ++i) oss << ' ';
     return oss.str();
