a9f0b0bc733e47cfee1e6bbe036c4a36621b54fd
Jinyu Chen <rzco0717@gmail.com>
2023-03-21 11:12:51 -0400

    practice @ Mar 19 (make-up)

---
+++ 1004_Max_Consecutive_Ones_III/prac4.cc
@@ -0,0 +1,14 @@
+#include "../utils.h"
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int longest = 0, zeroCount = 0;
+        for (auto lit = nums.cbegin(), rit = nums.cbegin(); rit < nums.cend(); ++rit) {
+            zeroCount += 1 - *rit;
+            while (zeroCount > k) zeroCount -= 1 - *lit++;
+            if (zeroCount <= k) longest = max(longest, (int)(rit - lit + 1));
+        }
+        return longest;
+    }
+};
